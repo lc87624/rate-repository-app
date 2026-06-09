6e75bd30b41e0750bcb01c337fadbcb299682a4e
lc87624 <lc87624@gmail.com>
2026-06-09 20:14:56 +0800

    feat: add SignIn component with form handling using Formik

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,77 @@
 import Text from './Text';
+import { Pressable, StyleSheet, TextInput, View } from 'react-native';
+import { useFormik } from 'formik';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#e1e4e8',
+    flex: 1,
+  },
+  form: {
+    backgroundColor: 'white',
+    padding: 18,
+    paddingTop: 30,
+    paddingBottom: 24,
+  },
+  input: {
+    borderColor: 'black',
+    borderRadius: 8,
+    borderWidth: 1,
+    color: theme.colors.textPrimary,
+    fontSize: 20,
+    marginBottom: 24,
+    paddingHorizontal: 18,
+    height: 64,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: theme.colors.primary,
+    borderRadius: 6,
+    justifyContent: 'center',
+    height: 64,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: theme.fontWeights.bold,
+  },
+});
+
+const onSubmit = (values) => {
+  console.log(values);
+};
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    onSubmit,
+  });
+  return (
+    <View style={styles.container}>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          value={formik.values.username}
+          onChangeText={formik.handleChange('username')}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          value={formik.values.password}
+          onChangeText={formik.handleChange('password')}
+        />
+        <Pressable style={styles.button} onPress={formik.handleSubmit}>
+          <Text style={styles.buttonText}>Sign in</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 };
 
 export default SignIn;
